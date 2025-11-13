cd989a835c3e908b753d319d1e0a900071470229
ramil <ramilshef@yandex.ru>
2025-11-13 09:16:37 +0300

    push5

--- PRAKTIKA4_KYRS_DEMO/Profil.xaml.cs
+++ PRAKTIKA4_KYRS_DEMO/Profil.xaml.cs
@@ -1,29 +1,47 @@
-﻿using System;
+﻿using PRAKTIKA4_KYRS_DEMO.BD;
+using PRAKTIKA4_KYRS_DEMO.BD.MODELS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PRAKTIKA4_KYRS_DEMO
 {
-    /// <summary>
-    /// Логика взаимодействия для Profil.xaml
-    /// </summary>
     public partial class Profil : Window
     {
         public Profil()
         {
             InitializeComponent();
+
+            if (Session.CurrentUser != null)
+            {
+                LoadProfile(Session.CurrentUser);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка: пользователь не найден. Пожалуйста, войдите снова.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
         }
 
+        private void LoadProfile(User user)
+        {
+            string roles = user.Role != null && user.Role.Any()
+                ? string.Join(", ", user.Role.Select(r => r.RoleName))
+                : "Нет ролей";
+
+            var profileData = new List<ProfileField>
+            {
+                new ProfileField { FieldName = "ID пользователя", FieldValue = user.ID.ToString() },
+                new ProfileField { FieldName = "ФИО", FieldValue = user.FIO },
+                new ProfileField { FieldName = "Email", FieldValue = user.Email },
+                new ProfileField { FieldName = "Дата рождения", FieldValue = user.DOB.ToString("dd.MM.yyyy") },
+                new ProfileField { FieldName = "Телефон", FieldValue = user.Phone },
+                new ProfileField { FieldName = "Роли", FieldValue = roles },
+            };
+
+            profileDataGrid.ItemsSource = profileData;
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
